893b5d2fe0ee4f8e71b08c0dc3ac71dd794f8b3a
Tomi Valkeinen <tomi.valkeinen@iki.fi>
2020-11-30 15:25:31 +0200

    py: cleanup __init__

--- src/py/pycamera/__init__.py
+++ src/py/pycamera/__init__.py
@@ -1,9 +1,5 @@
 from .pycamera import *
-from enum import Enum
-import os
-import struct
 import mmap
-
 
 def __FrameBuffer__mmap(self, plane):
 	return mmap.mmap(self.fd(plane), self.length(plane), mmap.MAP_SHARED, mmap.PROT_READ)
